b83f78e9d185f4f1d0f29a82702585ffc9b90607
Kiran Vadakkath <kiranvadakath@gmail.com>
2019-11-09 10:48:52 -0500

    Asynchronous calls test cases

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const functions = {
 	add: (num1, num2) => num1+num2,
 	isNull: () => null,
@@ -6,7 +7,11 @@ const functions = {
 		const user = { firstName: 'Kiran'};
 		user['lastName'] = 'VM';
 		return user;
-	}
+	},
+
+	fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+		.then(res => res.data)
+		.catch(err => 'error')
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -26,3 +26,36 @@ test('User should be Kiran VM object', () => {
 		lastName:'VM'
 	});
 });
+
+
+test('Should be under 1600', () => {
+	const load1 = 800;
+	const load2 = 700;
+	expect(load1 + load2).toBeLessThan(1600);
+});
+
+test('Should be under 1600', () => {
+	const load1 = 800;
+	const load2 = 700;
+	expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+//regex
+test('There is no I in team', () => {
+	expect('team').not.toMatch(/I/);
+});
+
+//arrays
+test('Admin should be in usernames', () => {
+	usernames = ['john','karen','admin'];
+	expect(usernames).toContain('admin');
+});
+
+//working with async data
+test('User fetched name should be Leanne Graham',() => {
+	expect.assertions(1);
+	return functions.fetchUser()
+		.then(data => {
+			expect(data.name).toEqual('Leanne Graham');
+		});
+});
